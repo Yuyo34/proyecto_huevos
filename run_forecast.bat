9c6649945b446a9e5dc529dd46da1642b39640e0
Yuyo <felipemasterg@gmail.com>
2025-09-15 00:14:48 -0300

    chore: run_forecast.bat actualizado (SARIMAX + USD Δlog + IMACEC YoY, lag=1)

--- run_forecast.bat
+++ run_forecast.bat
@@ -3,9 +3,9 @@ setlocal
 cd /d "%~dp0"
 py -m upgrade_precision.pipeline.pipeline_monthly_exog ^
   --target "data\precio_huevo_mensual.csv" ^
-  --usdclp "data\usdclp_dlog.csv" --ipc "data\ipc_yoy.csv" ^
+  --usdclp "data\usdclp_dlog.csv" --ipc "data\imacec_yoy.csv" ^
   --lags 1 --bt_init 12 --seasonality 12 --h 2 ^
   --no_boost ^
   --out "out\forecast_next2m.csv"
-echo Hecho. Salida: out\forecast_next2m.csv
+echo Hecho: out\forecast_next2m.csv
 endlocal
